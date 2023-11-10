fe5d30afce101f512d778c984b43be6a90e951ef
Robson <robsonribeirorra@gmail.com>
2023-11-10 17:35:33 -0300

    Initial

---
+++ desktop-app-GUI/src/mapapsii/MapaPSII.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mapapsii;
+
+import mapapsii.views.ResponsavelView;
+
+/**
+ *
+ * @author A
+ */
+public class MapaPSII {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ResponsavelView responsavelView = new ResponsavelView();
+        responsavelView.setVisible(true);
+    }
+    
+}
---
+++ desktop-app-GUI/src/mapapsii/dom/Responsavel.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mapapsii.dom;
+
+/**
+ *
+ * @author A
+ */
+public class Responsavel {
+    private String respName;
+    private String respTel;
+    private String respAdress;
+    private String respCPF;
+    private String respEmail;
+    private String respAge;
+    
+    private boolean isValid;
+    
+    public Responsavel(String respName, String respTel, String respAdress, String respCPF, String respEmail, String respAge) {
+        this.respName = respName;
+        this.respTel = respTel;
+        this.respCPF = respCPF;
+        this.respAdress = respAdress;
+        this.respEmail = respEmail;
+        this.respAge = respAge;
+    }
+    
+    public void validate() {
+        double age = Double.parseDouble(this.respAge);
+        if(age >= 18){
+            this.isValid = true;
+        } else {
+            this.isValid = false;
+        }
+    }
+
+    public String getRespName() {
+        return respName;
+    }
+
+    public String getRespTel() {
+        return respTel;
+    }
+
+    public String getRespAdress() {
+        return respAdress;
+    }
+
+    public String getRespCPF() {
+        return respCPF;
+    }
+
+    public String getRespEmail() {
+        return respEmail;
+    }
+
+    public String getRespAge() {
+        return respAge;
+    }
+
+    public boolean getIsValid() {
+        return isValid;
+    }
+    
+    
+}
